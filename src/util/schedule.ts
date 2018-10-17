d6d54bd9f4bf823fde03bf2bc0543904a291f948
b97tsk <b97tsk@gmail.com>
2018-10-17 13:01:48 +0800

    Update schedule.ts

--- src/util/schedule.ts
+++ src/util/schedule.ts
@@ -1,20 +1,15 @@
 export type ScheduleFunc = (callback: (...args: any[]) => void) => void
 
-export interface ScheduleObject extends ScheduleFunc {
-    replace(fn: ScheduleFunc): ScheduleFunc
+let scheduleFunc: ScheduleFunc = (callback: (...args: any[]) => void) => {
+    setTimeout(callback, 1)
 }
 
-export const schedule: ScheduleObject = (() => {
-    let scheduleFunc: ScheduleFunc = (callback: (...args: any[]) => void) => {
-        setTimeout(callback, 1)
-    }
-    const schedule: any = (callback: (...args: any[]) => void) => {
-        scheduleFunc(callback)
-    }
-    schedule.replace = (fn: ScheduleFunc): ScheduleFunc => {
-        const s = scheduleFunc
-        scheduleFunc = fn
-        return s
-    }
-    return schedule
-})()
+export const schedule = (callback: (...args: any[]) => void) => {
+    scheduleFunc(callback)
+}
+
+schedule.replace = (fn: ScheduleFunc): ScheduleFunc => {
+    const s = scheduleFunc
+    scheduleFunc = fn
+    return s
+}
